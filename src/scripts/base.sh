4a86407c575503935b9ec0c523cb3556f147273b
David J. Felix <felix.davidj@gmail.com>
2014-03-05 22:59:21 -0500

    Add SSH provision for vagrant user

--- src/scripts/base.sh
+++ src/scripts/base.sh
@@ -7,3 +7,6 @@ apt-get -y update
 apt-get -y upgrade
 apt-get -y install linux-headers-$(uname -r) build-essential
 
+# Setup sudo for vagrant
+sed -i -e 's/^%sudo.*/%sudo ALL=(ALL) NOPASSWD: ALL/' /etc/sudoers
+echo 'vagrant ALL=NOPASSWD:ALL' > /etc/sudoers.d/vagrant
