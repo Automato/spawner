cf709dfb62dcb6eabf8710480acda9e9eb594de1
David J. Felix <felix.davidj@gmail.com>
2014-03-09 23:50:07 -0400

    Update vagrant.sh

--- src/scripts/vagrant.sh
+++ src/scripts/vagrant.sh
@@ -2,8 +2,6 @@
 
 set -eu
 
-# Create the user vagrant with password vagrant
-useradd -G sudo -p $(perl -e'print crypt("vagrant", "vagrant")') -m -s /bin/bash -N vagrant
 
 # Set up sudo
 echo %vagrant ALL=NOPASSWD:ALL > /etc/sudoers.d/vagrant
@@ -17,6 +15,7 @@ wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/key
 chmod 600 /home/vagrant/.ssh/authorized_keys
 chown -R vagrant /home/vagrant/.ssh
 chmod -R go-rwsx /home/vagrant/.ssh
+rm -Rf /var/lib/dhcp/*
 
 # Install NFS for Vagrant
 apt-get -y update
