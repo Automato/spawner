04a2ec09c9bb17b669a238c7fc67afd95b0d91cb
David J. Felix <felix.davidj@gmail.com>
2014-04-28 13:33:57 -0400

    Update osx-10.9-dev-bootstrap.sh

--- osx-10.9-dev-bootstrap.sh
+++ osx-10.9-dev-bootstrap.sh
@@ -3,3 +3,5 @@
 xcode-select --install
 ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 brew install saltstack
+read -p "Enter salt master-server address: " address
+echo "master: $address" > /etc/salt/minion
