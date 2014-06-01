824718aa2d5b21d6ed21e108adb1fbb496d4e54f
David J. Felix <felix.davidj@gmail.com>
2014-06-01 01:03:55 -0400

    Make ubuntu update and upgrade first

--- ubuntu-14.04-dev-bootstrap.sh
+++ ubuntu-14.04-dev-bootstrap.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sudo apt update
+sudo apt upgrade -y
 sudo apt install -y salt-minion
 read -p "Enter salt master-server address: " address
 echo "master: $address" > /etc/salt/minion
