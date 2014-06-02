c8519487c31d297cd63c2a719aa044b7fcd3830f
David J. Felix <felix.davidj@gmail.com>
2014-06-02 12:23:14 -0400

    Add logging/debugging header

--- osx-10.9-dev-bootstrap.sh
+++ osx-10.9-dev-bootstrap.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# Set up some graceful failure and logging
+## Fail if a command fails or unknown variable is referenced
+set -eu
+## Fail a whole line preemptively if a command fails and pipes output to another command
+set -o pipefail
+## Beautify and push debug output to system log
+export PS4='+ ${FUNCNAME[0]:+${FUNCNAME[0]}():}line ${LINENO}: '
+syslogname="$(basename "$0")[$$]"
+exec 3<> >(logger -t "$syslogname")
+BASH_XTRACEFD=3
+echo "Tracing to syslog as $syslogname"
+unset syslogname
+debug() { echo "$@" >&3 }
+## Log all executed statements, after expansion
+set -x
+
 softwareupdate -i -a
 xcode-select --install
 ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
