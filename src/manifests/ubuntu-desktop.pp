6d98df82a38af6c5196f7683e3fd3cd58b8e334e
David J. Felix <felix.davidj@gmail.com>
2015-03-07 17:07:22 -0500

    Create ubuntu-desktop.pp

---
+++ src/manifests/ubuntu-desktop.pp
@@ -0,0 +1,47 @@
+node default {
+  package { 'git':
+    ensure => latest
+  }
+  package { 'vim':
+    ensure => latest
+  }
+  package { 'emacs':
+    ensure => latest
+  }
+  package { 'build-essential':
+    ensure => latest
+  }
+  package { 'llvm':
+    ensure => latest
+  }
+  package { 'clang':
+    ensure => latest
+  }
+  package { 'mosh':
+    ensure => latest
+  }
+  package { 'httpie':
+    ensure => latest
+  }
+  package { 'curl':
+    ensure => latest
+  }
+  package { 'zsh':
+    ensure => latest
+  }
+  package { 'screen':
+    ensure => latest
+  }
+  package { 'tmux':
+    ensure => latest
+  }
+  package { 'wget':
+    ensure => latest
+  }
+  package { 'sudo':
+    ensure => latest
+  }
+  package { 'ubuntu-desktop':
+    ensure => latest
+  }
+}
