efd86b7e4f769036117127ef2146c39aad6869fc
David J. Felix <felix.davidj@gmail.com>
2015-03-20 17:36:21 -0400

    Add some niceties

--- src/manifests/ubuntu-desktop.pp
+++ src/manifests/ubuntu-desktop.pp
@@ -1,47 +1,110 @@
 node default {
-  package { 'git':
-    ensure => latest
-  }
-  package { 'vim':
-    ensure => latest
-  }
-  package { 'emacs':
+  package { 'ack-grep':
     ensure => latest
   }
   package { 'build-essential':
     ensure => latest
   }
-  package { 'llvm':
+  package { 'byobu':
+    ensure => latest
+  }
+  package { 'bzr':
+    ensure => latest
+  }
+  package { 'bzr-git':
     ensure => latest
   }
   package { 'clang':
     ensure => latest
   }
-  package { 'mosh':
+  package { 'curl':
+    ensure => latest
+  }
+  package { 'elinks':
+    ensure => latest
+  }
+  package { 'emacs':
+    ensure => latest
+  }
+  package { 'gcc':
+    ensure => latest
+  }
+  package { 'git':
+    ensure => latest
+  }
+  package { 'git-bzr':
+    ensure => latest
+  }
+  package { 'git-svn':
+    ensure => latest
+  }
+  package { 'gradle':
     ensure => latest
   }
   package { 'httpie':
     ensure => latest
   }
-  package { 'curl':
+  package { 'links':
     ensure => latest
   }
-  package { 'zsh':
+  package { 'links2':
+    ensure => latest
+  }
+  package { 'llvm':
+    ensure => latest
+  }
+  package { 'mecurial':
+    ensure => latest
+  }
+  package { 'mercurial-git':
+    ensure => latest
+  }
+  package { 'mosh':
+    ensure => latest
+  }
+  package { 'oracle-java6-installer':
+    ensure => latest
+  }
+  package { 'oracle-java7-installer':
+    ensure => latest
+  }
+  package { 'oracle-java8-installer':
+    ensure => latest
+  }
+  package { 'oracle-java9-installer':
     ensure => latest
   }
   package { 'screen':
     ensure => latest
   }
-  package { 'tmux':
+  package { 'silversearcher-ag':
     ensure => latest
   }
-  package { 'wget':
+  package { 'squid3':
+    ensure => latest
+  }
+  package { 'subversion':
     ensure => latest
   }
   package { 'sudo':
     ensure => latest
   }
+  package { 'tmux':
+    ensure => latest
+  }
   package { 'ubuntu-desktop':
+    ensure => latest
+  }
+  package { 'vagrant':
+    ensure => latest
+  }
+  package { 'vim':
+    ensure => latest
+  }
+  package { 'wget':
+    ensure => latest
+  }
+  package { 'zsh':
     ensure => latest
   }
 }
