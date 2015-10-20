002840689161dfbb62d4525c0d55a8c0f4674451
David J. Felix <DavidJFelix@users.noreply.github.com>
2015-10-20 09:40:05 -0400

    Update build script.
    - Note, this still doesn't run without being explicitly pointed to the iso

--- Debian-Testing/build.sh
+++ Debian-Testing/build.sh
@@ -1,3 +1,20 @@
+sudo su -
 mkdir loopdir
-wget http://cdimage.debian.org/cdimage/stretch_di_alpha3/amd64/iso-cd/debian-stretch-DI-alpha3-amd64-netinst.iso
-sudo mount -o loop debian-stretch-DI-alpha3-amd64-netinst.iso loopdir
+mount -o loop your-image.iso loopdir
+mkdir cd
+rsync -a -H --exclude=TRANS.TBL loopdir/ cd
+umount loopdir
+mkdir irmod
+cd irmod
+gzip -d < ../cd/install/2.6/initrd.gz | \
+    cpio --extract --verbose --make-directories --no-absolute-filenames
+cp ../my_preseed.cfg preseed.cfg
+find . | cpio -H newc --create --verbose | \
+    gzip -9 > ../cd/install/2.6/initrd.gz
+cd ../
+rm -fr irmod/
+ cd cd
+ md5sum `find -follow -type f` > md5sum.txt
+ cd ..
+ genisoimage -o test.iso -r -J -no-emul-boot -boot-load-size 4 \
+ -boot-info-table -b isolinux/isolinux.bin -c isolinux/boot.cat ./cd
